b40aba38f1fb18c72c384ece468abd7f858f4cf8
shubhgosavi <55313358+shubhgosavi@users.noreply.github.com>
2020-12-24 16:05:15 +0530

    Update app.component.ts

--- app.component.ts
+++ app.component.ts
@@ -1,59 +1,70 @@
-<div class="row">
-  <div class="col-md-3"></div>
-  <div class="col-md-6">
-    <form [formGroup]="profileForm">
-      
-<table style="width:100%">
-  <tr>    
- <th> <label
-        >First Name:
-        <input type="text" formControlName="firstName" />
-      </label>
-      <label
-        >Last Name:
-        <input type="text" formControlName="lastName" />
-      </label>
+import { Component } from "@angular/core";
+import {
+  FormBuilder,
+  Validators,
+  ReactiveFormsModule,
+  FormsModule,
+  Validator,
+  FormControl,
+  FormGroup
+} from "@angular/forms";
+import { ProfileService } from "./profile.service";
 
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  title = "crud";
+  users;
+  usr = "";
+  constructor(
+    private fb: FormBuilder,
+    private profileService: ProfileService
+  ) {}
+  profileForm = this.fb.group({
+    firstName: [""],
+    lastName: [""]
+  });
+  deleteForm = this.fb.group({
+    delete: [""]
+  });
 
-      <button (click)="register()" class="btn btn-primary">Create</button></th></tr></table>
-    </form>
-   
+  updateForm = this.fb.group({
+    id: [""],
+    first: [""],
+    last: [""]
+  });
+  register() {
+    let data = this.profileForm.value;
 
-   <th> <button (click)="getUser()" class="btn btn-primary">Show User</button>
-    <li *ngFor="let user of users">
-      {{ user.user_id }}{{ " " }} {{ user.firstName }}{{ " "
-      }}{{ user.lastName }}
-    </li>
-    <div>
-      <form [formGroup]="deleteForm">
-        <input type="text" formControlName="delete" />
-        <button (click)="deleteUser()" class="btn btn-primary">
-          DeleteUser
-        </button>
-      </form>
-      <div>
-        <form [formGroup]="updateForm">
-                <table style="width:100%">
-                  <tr>
-                    <th>
-          <label
-            >user_id:
-            <input type="text" formControlName="id" />
-          </label></th><th>
-          <label
-            >First Name:
-            <input type="text" formControlName="first" />
-          </label></th>
-         <th> <label
-            >Last Name:
-            <input type="text" formControlName="last" />
-          </label></th>
-        <th>  <button (click)="updateUser()" class="btn btn-primary">
-            update
-          </button></th></tr></table>
-        </form>
-      </div>
-    </div>
-  </div>
-</div>
+    this.profileService.getData(data).subscribe(da => {
+      console.log("got result: " + da);
+    });
+  }
 
+  getUser() {
+    this.profileService.getUser().subscribe(data => {
+      console.log(data);
+      this.users = data;
+    });
+  }
+
+  deleteUser() {
+    this.usr = this.deleteForm.controls.delete.value;
+    let a = this.usr.split(",");
+    console.log(a);
+    let u = a;
+    this.profileService.deleteUser(u).subscribe(data => {
+      // console.log(data);
+    });
+  }
+  updateUser() {
+    let data = this.updateForm.value;
+
+    this.profileService.updateUser(data).subscribe(da => {
+      console.log("got result: " + da);
+    });
+  }
+}
